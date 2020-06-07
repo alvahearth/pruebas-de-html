a0301353530ef966b989c71be0e83093dfe230ad
alvahearth <tekken5lord@gmail.com>
2020-06-07 19:50:35 -0400

    cambios

---
+++ tercerapagina/hola.java
@@ -0,0 +1,10 @@
+public class hola {
+
+public static void main(String[] args) {
+    
+
+System.out.println("**********************************");
+System.out.println("*Bienvenidos a mi programa jajaxd*");
+System.out.println("**********************************");
+  }
+}
