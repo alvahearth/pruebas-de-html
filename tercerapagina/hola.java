30af256fb78f699d6c4b621eea7dbef06fd1c63a
alvahearth <alvahearth@users.github.com>
2020-06-07 20:00:40 -0400

    cambios

--- tercerapagina/hola.java
+++ tercerapagina/hola.java
@@ -1,10 +1,19 @@
 public class hola {
 
-public static void main(String[] args) {
-    
+  public static void main(String[] args) {
 
-System.out.println("**********************************");
-System.out.println("*Bienvenidos a mi programa jajaxd*");
-System.out.println("**********************************");
+    System.out.println("**********************************");
+    System.out.println("*Bienvenidos a mi programa jajaxd*");
+    System.out.println("**********************************");
+
+    var x = 3;
+    var y = 5;
+    System.out.println(x + y);
+    if (x == 2) {
+      System.out.println("hola, mamá estoy programando");
+    } else {
+      System.out.println("chao mamá");
+    }
+
   }
 }
